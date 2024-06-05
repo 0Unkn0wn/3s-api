e80aad5407c9db9230998035ddaa768aab7dfdd4
0Unkn0wn <mihnea.bastea@gmail.com>
2024-06-05 05:57:07 +0200

    added missing secret inside genenv

--- genenv.py
+++ genenv.py
@@ -6,6 +6,7 @@ def get_config_from_environment(env_content):
     env_content += "DB_URL={}\n".format(os.environ.get("DB_URL", ""))
     env_content += "DB_USER={}\n".format(os.environ.get("DB_USER", ""))
     env_content += "DB_PASSWORD={}\n".format(os.environ.get("DB_PASSWORD", ""))
+    env_content += "JWT_SECRET_KEY={}\n".format(os.environ.get("JWT_SECRET_KEY", ""))
     return env_content
 
 
