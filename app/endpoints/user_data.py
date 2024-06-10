a6ba3da5d02c7b46d9a71039e46e549eeba13150
0Unkn0wn <mihnea.bastea@gmail.com>
2024-06-10 23:31:22 +0200

    added endpoint to retrieve data from a table under the own-data route

--- app/endpoints/user_data.py
+++ app/endpoints/user_data.py
@@ -1,26 +1,16 @@
 from typing import Dict, Any
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from app.db import get_db, validate_schema_access, create_table_for_schema, get_schemas_and_tables, \
-    get_tables_for_schema
-from app.schemas.response_models import TablesResponse
+    get_tables_for_schema, get_data_for_table
+from app.schemas.response_models import TablesResponse, TableDataResponse
 from app.schemas.user import SystemUser
 from app.schemas.request_models import TableCreateRequest
 from app.utils import get_current_user
 
 router = APIRouter()
-
-
-@router.get("/tables", response_model=TablesResponse)
-def read_tables_for_schema(current_user: SystemUser = Depends(get_current_user)):
-    schema_name = f"user_own_data_{current_user.user_id}"
-    schemas_and_tables = get_schemas_and_tables([schema_name])
-    tables = get_tables_for_schema(schema_name, schemas_and_tables)
-    if "message" in tables:
-        raise HTTPException(status_code=404, detail=tables["message"])
-    return tables
 
 
 @router.post("/create_table/")
@@ -33,3 +23,25 @@ def create_table(
     validate_schema_access(schema_name, [schema_name])
     create_table_for_schema(db, schema_name, request.table_name, request.columns, request.primary_key)
     return {"message": f"Table {request.table_name} created successfully in schema {schema_name}"}
+
+
+@router.get("/tables", response_model=TablesResponse)
+def read_tables_for_schema(current_user: SystemUser = Depends(get_current_user)):
+    schema_name = f"user_own_data_{current_user.user_id}"
+    schemas_and_tables = get_schemas_and_tables([schema_name])
+    tables = get_tables_for_schema(schema_name, schemas_and_tables)
+    if "message" in tables:
+        raise HTTPException(status_code=404, detail=tables["message"])
+    return tables
+
+
+@router.get("/tables/{table_name}", response_model=TableDataResponse)
+def get_table_data(
+    table_name: str,
+    primary_key_value: Any = None,
+    limit: int = Query(None, description="Limit the number of rows returned"),
+    current_user: SystemUser = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    schema_name = f"user_own_data_{current_user.user_id}"
+    return get_data_for_table(db, schema_name, table_name, primary_key_value, limit)
